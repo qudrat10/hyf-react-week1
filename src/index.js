f0b095f896cfed080d67d1b6e1a569e4b9783b1c
Qudrtullah Ahmadi <qudrtullah@Admins-MacBook-Pro.local>
2018-05-19 11:38:21 +0200

    updated react-week1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { 
+    BrowserRouter as Router,
+     Route, 
+     Link } from "react-router-dom";
+import Header from './components/Header'  
+import About from './components/About'
+import Contact from './components/Contact'
+import Projects from './components/Projects'   
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+<Router>
+    <div>
+     <Route path="/" component={App} />
+     <Route exact path="/" component={About} />
+     <Route path="/Projects" component={Projects} />
+     <Route path="/Contact" component={Contact} />
+    </div>
+
+</Router>, 
+document.getElementById('root'));
+
